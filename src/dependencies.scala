ad7fd8c325fbebfeff08450e843e1dcd1724df43
tek <torstenschmits@gmail.com>
2015-10-08 18:24:00 +0200

    remove deepAarDeps completely

--- src/dependencies.scala
+++ src/dependencies.scala
@@ -210,14 +210,12 @@ object Dependencies {
         .get(struct.data)
         .getOrElse(Nil)
 
-    def aarDependencies = libraryDependencies filter(_.isAar)
-
     def dependsOnAar(aar: ModuleID) = {
       libraryDependencies exists(_.matches(aar))
     }
 
-    def deepAarDeps = {
-      deepDeps flatMap(_.aarDependencies)
+    def deepLibDeps = {
+      deepDeps flatMap(_.libraryDependencies)
     }
   }
 }
